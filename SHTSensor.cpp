ac259075e46adde433e28d27c53a953dce367c29
Koepel <Koepel@users.noreply.github.com>
2017-08-12 09:42:13 +0200

    No need to wait after Wire.requestFrom()

--- SHTSensor.cpp
+++ SHTSensor.cpp
@@ -73,14 +73,9 @@ bool SHTI2cSensor::readFromI2c(uint8_t i2cAddress,
 
   Wire.requestFrom(i2cAddress, dataLength);
 
-  // there should be no reason for this to not be ready, since we're using clock
-  // stretching mode, but just in case we'll try a few times
-  uint8_t tries = 1;
-  while (Wire.available() < dataLength) {
-    delay(1);
-    if (tries++ >= MAX_I2C_READ_TRIES) {
-      return false;
-    }
+  // check if the same number of bytes are received that are requested.
+  if (Wire.available() != dataLength) {
+    return false;
   }
 
   for (int i = 0; i < dataLength; ++i) {
